17cc7181eaca6c700068d7d08e1fb9f16e191e5c
Okurut Henry Kedi <okurut.henry@yahoo.com>
2026-03-25 10:40:01 +0300

    Added the spring project boilerplate code

---
+++ src/main/java/com/kedi/self_healing/SelfHealingApplication.java
@@ -0,0 +1,13 @@
+package com.kedi.self_healing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHealingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHealingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kedi/self_healing/SelfHealingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedi.self_healing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHealingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
