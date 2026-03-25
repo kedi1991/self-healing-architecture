3dd1ab2b69b7236dbd26268cd9b5520b5f950b77
Okurut Henry Kedi <okurut.henry@yahoo.com>
2026-03-25 11:23:13 +0300

    Add order controller logic

--- src/main/java/com/kedi/self_healing/OrderController.java
+++ src/main/java/com/kedi/self_healing/OrderController.java
@@ -1,5 +1,23 @@
 package com.kedi.self_healing;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/{userId}")
+    public String makeOrder(@PathVariable String userId){
+
+        String userDetail = restTemplate.getForObject("http://localhost:8080/users/" + userId, String.class);
+
+        return "ORDER PLACED FOR USER " + userDetail;
+    }
     
 }
